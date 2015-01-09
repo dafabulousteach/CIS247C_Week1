3a5824fd1b0ecdb11839d6cec0b797ae8e270d77
dafabulousteach <dafabulousteach@gmail.com>
2015-01-09 21:46:39 +0200

    Added methods

--- week1.c
+++ week1.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+string DisplayApplicationInformation();
+string DisplayDivider(string outputTitle);
+string GetInput(); //
+void TerminateApplication();
+    
 int main() {
-    printf("All Systems Go..");
+    DisplayApplicationInformation();
+    DisplayDivider("Start Program"); // start program
+    DisplayDivider("Get Name"); // getName
+    
     return 0;
 } // end of main
+
+string DisplayApplicationInformation() {
+    
+    cout << "Welcome to the Basic User Interface Program\nCIS247C, Week 1 Lab\nName: \nThis program accepts user input as a         string, then makes the appropriate data conversion."; 
+    
+} // end of DisplayApplicationInformation
+
+string DisplayDivider(string outputTitle) {
+    return outputTitle;
+} // end of DisplayDivider
+
+void TerminateApplication() {
+} // end Terminate Application
